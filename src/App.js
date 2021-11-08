7b2b4a6b36bbefccac6870e8958d63f1cb675be6
Olive105 <105.olivesoft@gmail.com>
2021-11-07 21:40:54 -0500

    feat - create task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 import Web3 from 'web3';
 import './App.css';
-import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config';
+import TodoList from './TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      account: '',
+      taskCount: 0,
+      tasks: [],
+      loading: true
+    };
+    this.createTask = this.createTask.bind(this);
+  }
+
   componentWillMount() {
     this.loadBlockchainData();
+  }
+
+  createTask(content) {
+    this.setState({ loading: true })
+    this.state.todoList.methods.createTask(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
 
   async loadBlockchainData() {
@@ -23,58 +43,17 @@ class App extends Component {
         tasks: [...this.state.tasks, task]
       })
     }
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = { 
-      account: '',
-      taskCount: 0,
-      tasks: [] 
-    };
-  }
+    this.setState({ loading: false });
+  }  
 
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small><a className="nav-link" href="#"><span id="account"></span></a></small>
-            </li>
-          </ul>
-        </nav>
-        <div className="container-fluid">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex justify-content-center mt-5">
-              <div id="loader" className="text-center">
-                <p className="text-center">Loading...</p>
-              </div>
-              <div id="content">
-                <form>
-                  <input id="newTask" type="text" className="form-control" placeholder="Add task..." required />
-                  <input type="submit" hidden="" />
-                </form>
-                <ul id="taskList" className="list-unstyled">
-                  { this.state.tasks.map((task, key) => {
-                    return(
-                      <div className="taskTemplate" className="checkbox" key={key}>
-                        <label>
-                          <input type="checkbox" />
-                          <span className="content">{task.content}</span>
-                        </label>
-                      </div>
-                    )
-                  })}
-                </ul>
-                <ul id="completedTaskList" className="list-unstyled">
-                </ul>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
+      <main role="main" className="col-lg-12 d-flex justify-content-center">
+        { this.state.loading
+          ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+          : <TodoList tasks={this.state.tasks} createTask={this.createTask} />
+        }
+      </main>
     );
   }  
 }
