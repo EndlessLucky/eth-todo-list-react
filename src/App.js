2b60da9174774e0a1d6fc557f58ca8119d7f7338
Olive105 <105.olivesoft@gmail.com>
2021-11-07 20:49:53 -0500

    feat - get account

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React, { Component } from 'react'
+import Web3 from 'web3';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { account: '' };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello, World!</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    );
+  }  
 }
 
 export default App;
